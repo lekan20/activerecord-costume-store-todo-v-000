86ae9d0b7549547af9f29a22180672d5a56b1bc1
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-04 22:13:09 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.integer :size
       t.string :image_url
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
